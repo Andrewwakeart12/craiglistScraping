ec0e2ea1690223f758ccb29aa11235ddae45cfd7
laOvejaBohem <andrewwake.art@gmail.com>
2021-02-05 12:00:47 -0400

    funcionalidad de acceso a formulario disponible

--- pageScraper.js
+++ pageScraper.js
@@ -1,8 +1,28 @@
 const scraperObject = {
-	url: 'https://rochester.craigslist.org/cpg/d/perkinsville-looking-for-partner-in-my/7262443714.html',
-async scraper(browser){
-let page= await browser.newPage();
-	console.log(this.url);
-	await page.goto(this.url); 
-}}
+	url: 'https://geo.craigslist.org/iso/us',
+	async scraper(browser){
+		let page= await browser.newPage();
+		console.log(this.url);
+		await page.goto(this.url); 
+
+		await page.waitForSelector('.geo-site-list');
+		let urls = await page.$$eval('section  ul > li' , links => {
+			links = links.map ( el => el.querySelector('a').href + 'search/cpg?is_paid=yes')
+			return links;
+		});
+
+		let pagePromise = (link)=> new Promise(async(resolve,reject) =>{
+		let dataObj = {};
+		let newPage = await browser.newPage();
+		await newPage.goto(link);
+		dataObj['gigTitle'] = await newPage.$eval('ul >.result-row > .result-info > h3 >  a', text => text.content);
+
+			resolve(dataObj);
+			await newPage.close();
+		});
+		for (link in urls){
+		let currentPageData = await pagePromise(urls[link]);
+			console.log(currentPageData);
+		}
+	}}
 module.exports = scraperObject;
